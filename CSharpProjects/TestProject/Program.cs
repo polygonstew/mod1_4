29cedabc944ddd887f198aff37641ae2736742e5
polygonstew <gamedevdata@gmail.com>
2026-03-31 13:32:04 -0400

    daysUntilExpiration corrections

--- CSharpProjects/TestProject/Program.cs
+++ CSharpProjects/TestProject/Program.cs
@@ -5,24 +5,27 @@ int discountPercentage = 0;
 // Your code goes here
 int due = daysUntilExpiration; 
 Console.WriteLine($"Your subscription expires in {due} days.");
-if (due > 1)
+if (due == 0)
    {
         Console.WriteLine("Your subscription has expired.");
    }
 else if ( due == 1 )
    {
-        Console.WriteLine(@"Your subscription expires within a day! n\ Renew now and save 20%!");
+        Console.WriteLine($"Your subscription expires within a day! \n Renew now and save 20%!");
+        discountPercentage = 20;
    }
 else if ( due <= 5)
    {
-        Console.WriteLine(@"Your subscription expires in 5 days. n\ Renew now and save 10%!");
+        Console.WriteLine($"Your subscription expires in 5 days. \n Renew now and save 10%!");
+        discountPercentage = 10;
+
    }
 else if (due <= 10)
    {
         
         Console.WriteLine("Your sub will expire soon. Renew now!");
    }
-else 
+if (discountPercentage > 0) 
    { 
-        Console.WriteLine("");
+        Console.WriteLine($"You are eligible for a {discountPercentage}% discount!");
    }
