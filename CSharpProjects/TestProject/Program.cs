54c1eab8c2a8aa130c2de5eebf7a84f2bd7c5641
polygonstew <gamedevdata@gmail.com>
2026-03-31 16:54:43 -0400

    NOT COMPLETE: progress 001

--- CSharpProjects/TestProject/Program.cs
+++ CSharpProjects/TestProject/Program.cs
@@ -1,59 +1,52 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
+﻿// Dice Roll
 
-string[] product = sku.Split('-');
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+// int players = 0;
+Console.WriteLine(" .----------------.  .----------------.  .----------------.  .----------------. ");
+Console.WriteLine("| .--------------. || .--------------. || .--------------. || .--------------. |");
+Console.WriteLine("| |  ________    | || |     _____    | || |     ______   | || |  _________   | |");
+Console.WriteLine("| | |_   ___ `.  | || |    |_   _|   | || |   .' ___  |  | || | |_   ___  |  | |");
+Console.WriteLine("| |   | |   `. \\ | || |      | |     | || |  / .'   \\_|  | || |   | |_  \\_|  | |");
+Console.WriteLine("| |   | |    | | | || |      | |     | || |  | |         | || |   |  _|  _   | |");
+Console.WriteLine("| |  _| |___.' / | || |     _| |_    | || |  \\ `.___.'\\  | || |  _| |___/ |  | |");
+Console.WriteLine("| | |________.'  | || |    |_____|   | || |   `._____.'  | || | |_________|  | |");
+Console.WriteLine("| |              | || |              | || |              | || |              | |");
+Console.WriteLine("| '--------------' || '--------------' || '--------------' || '--------------' |");
+Console.WriteLine(" '----------------'  '----------------'  '----------------'  '----------------' ");
+Console.WriteLine("");
+Console.WriteLine("Welcome to the Dice Roller!!! - Choose how many players! [1-4]");
+Console.Write("[ 1 - 4 ] ");
+//string pInput = Console.ReadLine();
+//int players = Convert.ToInt32(pInput);
 
-string type = "";
-string color = "";
-string size = "";
 
+Console.WriteLine($"You rolled a {roll}!");
 
-switch (product[0])
+Console.WriteLine("Ready Player One?");
+Console.WriteLine("Press any key to see the dice face...");
+//Console.ReadKey();
+
+switch (roll)
 {
-    case "01":
-        type = "Sweat shirt";
+    case 1:
+        Console.WriteLine(" ------- \n|       |\n|   o   |\n|       |\n ------- ");
         break;
-    case "02":
-        type = "T-Shirt";
+    case 2:
+        Console.WriteLine(" ------- \n|   o   |\n|       |\n|   o   |\n ------- ");
         break;
-    case "03":
-        type = "Sweat pants";
+    case 3:
+        Console.WriteLine(" ------- \n| o     |\n|   o   |\n|     o |\n ------- ");
         break;
-    default:
-        type = "Other";
+    case 4:
+        Console.WriteLine(" ------- \n| o   o |\n|       |\n| o   o |\n ------- ");
         break;
-}
-
-
-switch(product[1])
-{
-    case "BL":
-        color = "Black";
+    case 5:
+        Console.WriteLine(" ------- \n| o   o |\n|   o   |\n| o   o |\n ------- ");
         break;
-    case "MN":
-        color = "Maroon";
+    case 6:
+        Console.WriteLine(" ------- \n| o   o |\n| o   o |\n| o   o |\n ------- ");
         break;
-    default:
-        color = "White";
-        break;
-}
-
-switch(product[2])
-{
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
     
+}
 
-Console.WriteLine($"Product: {size} {color} {type}");
