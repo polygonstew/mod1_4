cedfbfb949e20627dc064b482db14286e37e934e
polygonstew <gamedevdata@gmail.com>
2026-03-31 14:48:55 -0400

    comparison ops and negation

--- CSharpProjects/TestProject/Program.cs
+++ CSharpProjects/TestProject/Program.cs
@@ -1,31 +1,10 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿Random rnum = new Random();
+int total = 0;
+int bob = rnum.Next(100);
 
-// Your code goes here
-int due = daysUntilExpiration; 
-Console.WriteLine($"Your subscription expires in {due} days.");
-if (due == 0)
-   {
-        Console.WriteLine("Your subscription has expired.");
-   }
-else if ( due == 1 )
-   {
-        Console.WriteLine($"Your subscription expires within a day! \n Renew now and save 20%!");
-        discountPercentage = 20;
-   }
-else if ( due <= 5)
-   {
-        Console.WriteLine($"Your subscription expires in 5 days. \n Renew now and save 10%!");
-        discountPercentage = 10;
-
-   }
-else if (due <= 10)
-   {
-        
-        Console.WriteLine("Your sub will expire soon. Renew now!");
-   }
-if (discountPercentage > 0) 
-   { 
-        Console.WriteLine($"You are eligible for a {discountPercentage}% discount!");
-   }
+Console.WriteLine($"Bob is {bob}");
+bob = rnum.Next(100);
+Console.Write("Name, please...");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}... is that Latin?");
+Console.Write($"Well {name}, your number is {bob}");
