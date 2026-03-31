7273abd26e3fdc189b5ad1009791081e8bdcb797
polygonstew <gamedevdata@gmail.com>
2026-03-31 15:15:49 -0400

    decision logic challenge

--- CSharpProjects/TestProject/Program.cs
+++ CSharpProjects/TestProject/Program.cs
@@ -1,14 +1,58 @@
-﻿// Conditional operator challenge
-Random randy = new Random();
-int rick = randy.Next(2) > 0 ? 10 : 20;
-if (rick == 10) {Console.WriteLine("Heads");} else {Console.WriteLine("Tails");} 
+﻿
+// Decision logic challenge
 
+string permission = "Admin|Manager";
+int level = 55;
 
+if (permission.Contains("Admin") && level >= 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin") && level < 55)
+{ 
+    Console.WriteLine("Welcome, Admin user.");
+}
 
-/*
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (permission.Contains("Manager") && level > 20)
+{ 
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+if (!permission.Contains("Manager") && !permission.Contains("Admin"))
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
 
-int saleAmount = 1001;
-int discount = saleAmount > 1000 ? 100 : 50;
-Console.WriteLine($"Discount: {discount}");
+// LEARN: Correct Code
+//string permission = "Admin|Manager";
+//int level = 53;
 
-*/
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
